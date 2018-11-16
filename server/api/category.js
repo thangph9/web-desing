568d7d94a95e52f0ac6c27d264a6fc306e7a40e3
trjvjp97 <trjvjp97@gmail.com>
2018-11-16 18:50:44 +0700

    123

--- server/api/category.js
+++ server/api/category.js
@@ -1,33 +1,43 @@
-var express     = require('express')
-var moment      = require('moment');
-const jwt       = require('jsonwebtoken');
-const fs        = require('fs');
-const async           = require("async");
-const bcrypt          = require("bcryptjs");
-const Uuid            = require("cassandra-driver").types.Uuid;
-const models          = require("../settings");
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
+var express = require('express');
+var moment = require('moment');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const async = require('async');
+const bcrypt = require('bcryptjs');
+const Uuid = require('cassandra-driver').types.Uuid;
+const models = require('../settings');
 
-var publicKEY  = fs.readFileSync('./ssl/jwtpublic.pem', 'utf8');  
-var router = express.Router()
-router.get('/category',function(req,res,next){
-    let result=[];
-    
-    async.series([
-        function(callback){
-            callback(null,null);    
-        },
-        function(callback){
-            models.instance.product_detail({},function(err,res){
-                
-                if(res){
-                    result=res;
-                }
-                callback(err,null);
-            })
-        },
-    ],function(err,result){
-        if(err) return res.send({status: 'error'});
-        res.send({status: 'oke',data: })
-    })
-})
-module.exports = router
+var publicKEY = fs.readFileSync('./ssl/jwtpublic.pem', 'utf8');
+var router = express.Router();
+router.get('/category', function(req, res, next) {
+  let result = [];
+
+  async.series(
+    [
+      function(callback) {
+        callback(null, null);
+      },
+      function(callback) {
+        models.instance.product_detail({}, function(err, res) {
+          if (res) {
+            result = res;
+          }
+          callback(err, null);
+        });
+      },
+    ],
+    function(err, results) {
+      if (err) return res.send({ status: 'error' });
+      res.send({ status: 'oke', data: result });
+    }
+  );
+});
+module.exports = router;
--- server/api/product.js
+++ server/api/product.js
@@ -1,160 +1,216 @@
-var moment      = require('moment');
-const jwt       = require('jsonwebtoken');
-const fs        = require('fs');
-const async           = require("async");
-const bcrypt          = require("bcryptjs");
-const Uuid            = require("cassandra-driver").types.Uuid;
-const models          = require("../settings");
-var publicKEY  = fs.readFileSync('./ssl/jwtpublic.pem', 'utf8');  
-var express     = require('express')
+/* eslint-disable vars-on-top */
+/* eslint-disable object-shorthand */
+/* eslint-disable prefer-template */
+/* eslint-disable consistent-return */
+/* eslint-disable no-shadow */
+/* eslint-disable dot-notation */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+/* eslint-disable prefer-const */
+/* eslint-disable import/order */
+/* eslint-disable import/newline-after-import */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
+var moment = require('moment');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const async = require('async');
+const bcrypt = require('bcryptjs');
+const Uuid = require('cassandra-driver').types.Uuid;
+const models = require('../settings');
+var publicKEY = fs.readFileSync('./ssl/jwtpublic.pem', 'utf8');
+var express = require('express');
 const sharp = require('sharp');
-function productList(req,res){
-    let results={};
-    
-    async.series([
-        function(callback){
-            callback(null,null);    
-        },
-        function(callback){
-            models.instance.category.find({$solr_query:'{"q": "category: *ef4a584e-3497-4b55-8991-55146d5a4757*"}'},{select: ['title','thumbnail']},function(err,res){
-                if(res){
-                    results['news']=res;
-                }
-                callback(err,null);
-            })
-        },
-        function(callback){
-            models.instance.category.find({$solr_query:'{"q": "category: *af739c5a-fa25-44bf-bc83-56fadcb1967f*"}'},{select: ['title','thumbnail']},function(err,res){
-                if(res){
-                    results['days']=res;
-                }
-                callback(err,null);
-            })
-        },
-        function(callback){
-             models.instance.category.find({$solr_query:'{"q": "category: *08ecb1e-cabf-4328-9ddc-011ca55a156d*"}'},{select: ['title','thumbnail']},function(err,res){
-                if(res){
-                    results['hotnew']=res;
-                }
-                callback(err,null);
-            })
-        },
-        function(callback){
-             models.instance.category.find({$solr_query:'{"q": "category: *07081437-d862-48d0-9987-4f656bd2de30*"}'},{select: ['title','thumbnail']},function(err,res){
-                if(res){
-                    results['bestSeller']=res;
-                }
-                callback(err,null);
-            })
-        }
-    ],function(err,result){
-        if(err) return res.send({status: 'error'});
-        res.send({status: 'ok',data: results})
-    })
-}
-function getRaito(req,res){
-    let raito={};
-    async.series([
-       function(callback){
-            callback(null,null);    
-       },
-       function(callback){
-           models.instance.currency_raito.find({},function(err,items){
-               if(items){
-                   raito=items;
-               }
-               callback(err,null);
-           });
-       }
-    ],function(err,result){
-        if(err) return res.send({status: 'error'});
-        res.send({status: 'ok',data:raito});
-    });
-}
+function productList(req, res) {
+  let results = {};
 
-function productDetail(req,res){
-    let results={};
-    let productid='';
-    let raito={};
-    async.series([
-        function(callback){
-            try{
-                let rawImage=req.body.productid;
-                let uuid=rawImage.substring(0,7)+'-'+rawImage.substring(7,11)+'-'+rawImage.substring(11,15)+'-'+rawImage.substring(15,20)+'-'+rawImage.substring(20,32)
-                
-                productid=models.uuidFromString(uuid);
-            }catch(e){
-                
-                return res.send({status: 'error_invalid'})
+  async.series(
+    [
+      function(callback) {
+        callback(null, null);
+      },
+      function(callback) {
+        models.instance.category.find(
+          { $solr_query: '{"q": "category: *ef4a584e-3497-4b55-8991-55146d5a4757*"}' },
+          { select: ['title', 'thumbnail'] },
+          function(err, res) {
+            if (res) {
+              results['news'] = res;
             }
-            callback(null,null);    
-        },
-        function(callback){
-            models.instance.product_detail.find({productid: productid},function(err,res){
-                if(res){
-                    results=res[0];
-                }
-                callback(err,null);
-            })
-        },
-        function(callback){
-            if(results && results.currency){
-                models.instance.currency_raito.find({currency: results.currency},function(err,items){
-                   if(items){
-                       let n=JSON.stringify(results);
-                       results=JSON.parse(n);
-                       results['_price']=items[0].raito*results.price;
-                       results['_sale_price']=items[0].raito*results.sale_price;
-                   }
-                   callback(err,null);
-               });
-                
-            }else{
-                callback(null,null);
-            }        
+            callback(err, null);
+          }
+        );
+      },
+      function(callback) {
+        models.instance.category.find(
+          { $solr_query: '{"q": "category: *af739c5a-fa25-44bf-bc83-56fadcb1967f*"}' },
+          { select: ['title', 'thumbnail'] },
+          function(err, res) {
+            if (res) {
+              results['days'] = res;
+            }
+            callback(err, null);
+          }
+        );
+      },
+      function(callback) {
+        models.instance.category.find(
+          { $solr_query: '{"q": "category: *08ecb1e-cabf-4328-9ddc-011ca55a156d*"}' },
+          { select: ['title', 'thumbnail'] },
+          function(err, res) {
+            if (res) {
+              results['hotnew'] = res;
+            }
+            callback(err, null);
+          }
+        );
+      },
+      function(callback) {
+        models.instance.category.find(
+          { $solr_query: '{"q": "category: *07081437-d862-48d0-9987-4f656bd2de30*"}' },
+          { select: ['title', 'thumbnail'] },
+          function(err, res) {
+            if (res) {
+              results['bestSeller'] = res;
+            }
+            callback(err, null);
+          }
+        );
+      },
+    ],
+    function(err, result) {
+      if (err) return res.send({ status: 'error' });
+      res.send({ status: 'ok', data: results });
+    }
+  );
+}
+function getRaito(req, res) {
+  let raito = {};
+  async.series(
+    [
+      function(callback) {
+        callback(null, null);
+      },
+      function(callback) {
+        models.instance.currency_raito.find({}, function(err, items) {
+          if (items) {
+            raito = items;
+          }
+          callback(err, null);
+        });
+      },
+    ],
+    function(err, result) {
+      if (err) return res.send({ status: 'error' });
+      res.send({ status: 'ok', data: raito });
+    }
+  );
+}
+
+function productDetail(req, res) {
+  let results = {};
+  let productid = '';
+  let raito = {};
+  async.series(
+    [
+      function(callback) {
+        try {
+          let rawImage = req.body.productid;
+          let uuid =
+            rawImage.substring(0, 7) +
+            '-' +
+            rawImage.substring(7, 11) +
+            '-' +
+            rawImage.substring(11, 15) +
+            '-' +
+            rawImage.substring(15, 20) +
+            '-' +
+            rawImage.substring(20, 32);
+
+          productid = models.uuidFromString(uuid);
+        } catch (e) {
+          return res.send({ status: 'error_invalid' });
         }
-    ],function(err,result){
-        if(err) return res.send({status: 'error'});
-        res.send({status: 'ok',data: results})
-    })
+        callback(null, null);
+      },
+      function(callback) {
+        models.instance.product_detail.find({ productid: productid }, function(err, res) {
+          if (res) {
+            results = res[0];
+          }
+          callback(err, null);
+        });
+      },
+      function(callback) {
+        if (results && results.currency) {
+          models.instance.currency_raito.find({ currency: results.currency }, function(err, items) {
+            if (items) {
+              let n = JSON.stringify(results);
+              results = JSON.parse(n);
+              results['_price'] = items[0].raito * results.price;
+              results['_sale_price'] = items[0].raito * results.sale_price;
+            }
+            callback(err, null);
+          });
+        } else {
+          callback(null, null);
+        }
+      },
+    ],
+    function(err, result) {
+      if (err) return res.send({ status: 'error' });
+      res.send({ status: 'ok', data: results });
+    }
+  );
 }
-function productCategory(req,res){
-    let results={};
-    let PARAMS_IS_VALID={};
-    const params=req.query;
-    async.series([
-        function(callback){
-            let rawImage=params.nodeid;
-            let uuid=rawImage.substring(0,7)+'-'+rawImage.substring(7,11)+'-'+rawImage.substring(11,15)+'-'+rawImage.substring(15,20)+'-'+rawImage.substring(20,32)
-            PARAMS_IS_VALID['nodeid']=uuid;
-            callback(null,null);    
-        },
-        function(callback){
-            models.instance.category.find({$solr_query:'{"q": "category: *'+PARAMS_IS_VALID['nodeid']+'*"}'},{select: ['title','thumbnail']},function(err,res){
-                if(res){
-                    results['news']=res;
-                }
-                callback(err,null);
-            })
-        },
-    ],function(err,result){
-        if(err) return res.send({status: 'error'});
-        res.send({status: 'ok',data: results})
-    })
+function productCategory(req, res) {
+  let results = {};
+  let PARAMS_IS_VALID = {};
+  const params = req.query;
+  async.series(
+    [
+      function(callback) {
+        let rawImage = params.nodeid;
+        let uuid =
+          rawImage.substring(0, 7) +
+          '-' +
+          rawImage.substring(7, 11) +
+          '-' +
+          rawImage.substring(11, 15) +
+          '-' +
+          rawImage.substring(15, 20) +
+          '-' +
+          rawImage.substring(20, 32);
+        PARAMS_IS_VALID['nodeid'] = uuid;
+        callback(null, null);
+      },
+      function(callback) {
+        models.instance.category.find(
+          { $solr_query: '{"q": "category: *' + PARAMS_IS_VALID['nodeid'] + '*"}' },
+          { select: ['title', 'thumbnail'] },
+          function(err, res) {
+            if (res) {
+              results['news'] = res;
+            }
+            callback(err, null);
+          }
+        );
+      },
+    ],
+    function(err, result) {
+      if (err) return res.send({ status: 'error' });
+      res.send({ status: 'ok', data: results });
+    }
+  );
 }
-function bestSeller(req,res){
-    
-}
-function dealday(req,res){
-    
-}
-function hotnew(req,res){
-    
-}             
+function bestSeller(req, res) {}
+function dealday(req, res) {}
+function hotnew(req, res) {}
 
-
-var router = express.Router()
-router.get('/list',productList);
-router.post('/DT',productDetail);
-router.get('/CT',productCategory);
-module.exports = router
+var router = express.Router();
+router.get('/list', productList);
+router.post('/DT', productDetail);
+router.get('/CT', productCategory);
+module.exports = router;
