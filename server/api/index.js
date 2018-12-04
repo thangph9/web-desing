7f2518758625ccdc57b7ab7a02d2ad7cfff2b357
trjvjp97 <trjvjp97@gmail.com>
2018-12-04 19:55:26 +0700

    123

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,12 @@
-var express     = require('express')
-var router = express.Router()
-router.use('/order',require('./order'))
-router.use('/user',require('./user'))
-router.use('/product',require('./product'))
-router.use('/global',require('./global'))
-router.use('/category',require('./category'))
-module.exports = router
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/newline-after-import */
+/* eslint-disable no-var */
+var express = require('express');
+var router = express.Router();
+router.use('/order', require('./order'));
+router.use('/user', require('./user'));
+router.use('/product', require('./product'));
+router.use('/global', require('./global'));
+router.use('/category', require('./category'));
+router.use('/authentication', require('./authentication'));
+module.exports = router;
