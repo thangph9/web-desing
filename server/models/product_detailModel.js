c9a00abac99f231793243371a78921d3ec7d1f3f
trjvjp97 <trjvjp97@gmail.com>
2018-12-22 14:34:35 +0700

    123

--- server/models/product_detailModel.js
+++ server/models/product_detailModel.js
@@ -1,53 +1,53 @@
-module.exports={
-    fields:{
-        productid : 'uuid',
-        asin: 'text', 
-        title : 'text', 
-        price : 'float',
-        sale    : 'float',
-        sale_price: 'float',
-        createat    : 'timestamp',
-        sale            : 'int',
-        thumbnail      : 'text',
-        reviews      : 'text',
-        seller     : 'text',
-        brand      : 'text',
-        style      : 'text',
-        star                 : 'text',
-        color                : 'text',
-        size                 : 'text',
-        currency    :'text',
-        amount      :'int',
-        materials_use       : 'text',
-        node_name          : 'text',
-        nodeid                 :{
-            type: "set",
-            typeDef:"<text>"
-        },
-        description    : 'text',
-        desc_detail    : 'text',
-        death_clock    : {
-            type: "map",
-            typeDef: "<text,timestamp>"
-        },
-        infomation      : { 
-            type: "map",
-            typeDef: "<text, text>"},
-        image_large     :{
-            type: "set",
-            typeDef:"<text>"
-        },
-        image_huge  :{
-            type: "set",
-            typeDef: "<text>"
-        },
-        image_small:{
-            type: "set",
-            typeDef: "<text>"
-        },
-        seo_link : 'text',
-        meta : 'text',
-        meta_description : 'text',
+module.exports = {
+  fields: {
+    productid: 'uuid',
+    asin: 'text',
+    title: 'text',
+    price: 'float',
+    sale: 'float',
+    sale_price: 'float',
+    createat: 'timestamp',
+    thumbnail: 'text',
+    reviews: 'text',
+    seller: 'text',
+    brand: 'text',
+    style: 'text',
+    star: 'text',
+    color: 'text',
+    size: 'text',
+    currency: 'text',
+    amount: 'int',
+    materials_use: 'text',
+    node_name: 'text',
+    nodeid: {
+      type: 'set',
+      typeDef: '<text>',
     },
-    key:[["productid"]] ,
-} 
+    description: 'text',
+    desc_detail: 'text',
+    death_clock: {
+      type: 'map',
+      typeDef: '<text,timestamp>',
+    },
+    infomation: {
+      type: 'map',
+      typeDef: '<text, text>',
+    },
+    image_large: {
+      type: 'set',
+      typeDef: '<text>',
+    },
+    image_huge: {
+      type: 'set',
+      typeDef: '<text>',
+    },
+    image_small: {
+      type: 'set',
+      typeDef: '<text>',
+    },
+    seo_link: 'text',
+    meta: 'text',
+    meta_description: 'text',
+  },
+  key: [['productid']],
+};
