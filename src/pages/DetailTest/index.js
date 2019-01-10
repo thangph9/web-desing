61115c0ea146dc717b560b37697abce1a698b646
trjvjp97 <trjvjp97@gmail.com>
2019-01-10 16:32:27 +0700

    123

--- src/pages/DetailTest/index.js
+++ src/pages/DetailTest/index.js
@@ -439,7 +439,7 @@ class DetailTest extends PureComponent {
           <img
             key={i}
             className={`${styles['slick-slide']} ${styles['images-slider__image___wW9Yw']}`}
-            src={`/images/f/${image_huge[this.state.index]}`}
+            src={`/images/f/${image_huge[this.state.index].replace(/\-/g, '')}`}
             style={{ outline: 'none', width: 447 }}
           />
         );
